6b5f8e12b078947b4d5980d8b1c80deca09a6a5f
chkiran <chkiran@synamedia.com>
2019-03-22 15:27:51 +0530

    added stable graphs

--- UPC_script.py
+++ UPC_script.py
@@ -107,6 +107,28 @@ def PreParseXml(xml_string):
     return xml_string
 
 
+
+def AddLineChart(writer, name, row_len):
+        xlworkbook = writer.book
+        xlworksheet = writer.sheets[name]
+        print(xlworksheet)
+        row_len = str(row_len)
+        chart = xlworkbook.add_chart({'type': 'line'})
+        chart.add_series({
+            'values': '='+name+'!$D$2:$D$'+row_len+'\'',
+            'name': 'used'
+            })
+        chart.add_series({
+            'values': '='+name+'!$E$2:$E$'+row_len+'\'',
+            'name': 'pool size'
+            })
+        chart.set_x_axis({'name': 'Time', 'position_axis': 'on_tick'})
+        chart.set_y_axis({'name': 'size'})
+        chart.set_title({'name': name})
+        print(chart)
+        print(name)
+        xlworksheet.insert_chart('I2', chart)
+
 file = open(In_path, mode='r')
 xml_string = file.read()
 file.close()
@@ -126,8 +148,7 @@ for Worksheet in Workbook:
 for Worksheet in Workbook:
     name = Worksheet[1]
     row_len = len(Worksheet[0])+1
-    gp.graphs.AddLineChart(writer, name, row_len)
-
+    AddLineChart(writer, name, row_len)
 
 #
 #xlworkbook = writer.book
